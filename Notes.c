5617334a553a74279f67cb5884db6d44236b131b
MassivePYR <jeovah02@gmail.com>
2023-01-04 16:33:20 -0300

    Create Notes.c

---
+++ Notes.c
@@ -0,0 +1,232 @@
+//Welcome to my anotations, if got in here without my permission... i hope you get a better life than being a homelass, god bless you and stop doing this kind of childish thing.. 
+//I hope your IP MAC dont get two times in my log and you know why... ;)
+____________________________________________________________________________________________________________________________________________________________________________________
+
+(Home Academia project:){		
+							
+		08 anilhas 1kg				. Construir uma estrutura pra isso.
+		2 anilhas 5kg				. Comprar uma máquina multimodal em exercícios.
+		2 anilhas 10kg				. Comprar um Quadro Branco pra anotar os treinos.
+		4 anilhas 20kg				. Set a Rotine about it, and run it.
+		----------------			. Comprar suplementos alimenticíos.(whey,creatine,zma,vitamins)
+		Barra Reta				
+		Barra W					
+		----------------		
+
+}
+
+(Leitor de livros:){
+		.Ideias:
+			. oferece espaço para anotações não atreladas a pagina mas sim o livro em si.
+			. suporte a marcadores personalizáveis
+			. fazer em javascript.
+}
+
+____________________________________________________________________________________________________________________________________________________________________________________
+
+Textos, anotações e mais:
+
+✪  MassivePYR -iwnl-
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+(Uncanny Valley:){
+
+	Em 1970 o professor e pesquisador de robótica, masahiro mori, escreveu sobre como reagimos a robos e animações com caracteristicas humanas,
+    ele mostrou que quando tentamos reproduzir caracteristicas humanas em animações e robotica os resultados inspiram simpatia só até certo ponto.
+    Quando as simulações ganham caracteriscas próximas demais as nossas,
+    mas ainda fica claro que não é um humano a figura pode causar desconforto e até medo em alguns casos.
+    Reagimos assim porquê nosso cerebro evoluiu para identificar outros humanos através das expressões faciais e corporais.
+    Quando vemos um ser que fica no limite, entra em ação um mecanismo pra nos proteger e esse momento foi chamado de uncanny valley, ou vale do esquisito. 
+    Nos ultimos 40 anos o desafio da computação tem sido ultrapassar esse vale, tin-tin,
+    wall-e e toystory não geram esse estranhamento justamente por não tentarem se parecer com um humano fielmente e se focarem em outras caracteristicas pra gerar simpatia,
+    como as emoções.
+    O segredo pra ultrapassar esse vale está nos olhos, criações como essa da actvision se preocupam bastante com eles, 
+
+    bem realista né? só a boca ainda parece estranha. (comentário)
+
+    já estamos bem distantes do ponto mais profundo do vale,
+    falta muito pouco pra chegar do outro lado quando uma foto ou uma animação grafica realista serâo indistiguiveis uma da outra,
+    um outro exemplo que passa muito perto de uma realidade é o jogo Detroit, que questiona o que é a inteligencia e como avaliar ser submisso ou escravo,
+    não será valioso só para filmes e games, será possivel conversarmos com avatares identicos a nos mesmos por exemplo, mas pra isso ainda falta passar de vez o vale. 
+
+    Empresas como a Boston Dynamics tem se focado no aprimoramento das movimentaçôes 
+}
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+(Novel Trial:){
+
+Mundo... uma Terra paralela ou outro mundo?... isekai?
+-24/09/2022 - Acho que uma historia "facil" de se escrever seria a historia de um alguem da epoca atual com formação/profissão definida reincarna no
+passado. penso dessa forma pois seria muito facil pensar as mudanças que realmente ocorreriam com alguem com um conhecimento elevado atual -2022- 
+indo de volta no tempo e aplicando essas mudanças. exemplo: 
+um politico estrategista volta no tempo,
+um medico cirurgiao volta no tempo,
+um estudioso volta no tempo,
+um professor de fisica volta no tempo,
+um biologo volta no tempo,
+um botanico volta no tempo,
+um inventor volta no tempo,
+um lutier volta no tempo,
+um bodybuilder volta no tempo,
+um crossfiteiro volta no tempo,
+um vendedor volta no tempo,
+um "explorador" volta no tempo (no sentido de ser alguem que sempre tenta tirar vantagem de algo de outrem),
+um engenheiro volta no tempo,
+um juiz/advogado volta no tempo,
+
+
+ideia suprema - um(a) "deus(a)" tenta pegar uma pessoa de cada profissão do presente os trás a outro mundo como "heróis" mesmo que com pouco ou nenhum poder pra ver se eles sobrevivem os tempos antigos.
+pode nessa ideia ter alguns que falham por serem muito preguiçosos/iguinorantes/ou por não ter aptidão física
+assim, alguns deles tambem vão se juntando e formando aliaças fortes que aceleram o processo de evolução
+
+----
+1º um mundo onde todos podem ser fortes só dependendo de sua força de vontade. // Gosto da ideia de cultivo espiritual
+2º gosto da perspectiva dos pontos de distribuição. // relacionado à habilidades? dinheiro? sistema de compra de algo?
+O que faz os personagem principal ser diferente?.. sua personalidade?
+personagem principal 
+Alguma coisa relacionada com futuro até o momento (Mirai+mingoto{muito bom}) Miragoto Shiroyama
+//Seria legal uma transformação relacionada ao poder, algo como ficar com os cabelos brancos assim como o nome
+Familia Shiroyama - renomada porém não gosta de se mostrar... tem muito poder escondido - talvez ter um irmão mais velho seria legal... alguem confiável
+Externo - Provavelmente (moreno, esguio porém com porte físico... só o suficiente pra não aparecer demais, não muito belo.. um pouco melhor que um comum)
+Interno - Inteligente, esperto, calculista, frio aos externos, confiável
+Sempre foi o mais inteligente mas por querer se proteger dos olhos dos outros ele sempre se contém pra não aparecer muito a vista dos outros.
+Se coloca sempre em provas como o segundo.
+Nunca chama atenção demais por querer.
+Ama música e esportes com o corpo
+Gosta muito de aprender coisas que são maquinicáveis
+Tem dificil atração por alguem... mas... não tem problemas na conquista se quiser.
+
+}
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+(Little Something:){
+-------------------------||
+-------------------------||
+------9-----7-----10-----||   Riff saltos de Terça
+----10-----9-----7-------||		made by
+--12-----10-----8--------||       Jeovah Reis
+-------------------------||
+}
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+(Anotações DPS:){
+DPS: Fecha curto pra que a corrente elétrica não entre na rede da casa
+
+	Tipo 1
+		Pra tomar porrada.
+
+	Tipo 2
+		Residual.
+
+	Tipo 3
+		Sensível... ex: Tomada pra coisas que tu não quer que queime nem a pau.
+		Geralmente usa pra uma única tomada ou uma tomada que não pede muita tensão.
+}
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+(Thierson 28-lista 5){
+
+#include <stdio.h>
+#include <stdlib.h> 
+int i, j;
+void leitura (int *, int);
+void uniao (int *, int, int*, int);
+void intersecao (int *, int, int *, int, int *, int *);
+ 
+int main (){
+ 
+    int con = 0;
+    int tA, tB, tI;
+    while(1){
+        scanf("%d", &tA);
+        if(tA>=1 && tA<=100){
+            break;
+        }
+    }
+    while(1){
+        scanf("%d", &tB);
+        if(tB>=1 && tB<=100){
+            break;
+        }
+    }
+    tI = tA + tB;
+    int A[tA], B[tB], I[tI];
+    leitura(A, tA);
+    leitura(B, tB);
+
+    //calcula a interseção
+    intersecao (A, tA, B, tB, I, &con);
+
+    //calcula e imprime a união
+    uniao(A, tA, B, tB);
+
+    //imprime a interseção
+    printf("(");
+    for(i=0; i<con; i++){
+        if(i!=0 && i+1<=con){
+            printf(",");
+        } 
+        printf("%d", I[i]);
+    }
+    printf(")");
+}
+ 
+void leitura (int *V, int n){
+    for(i=0; i<n; i++){
+        scanf("%d", V+i);
+        for(j=0; j<i; j++){
+            if(V[j] == V[i]){
+                i--;
+                break;
+            }
+        }
+    }
+}
+void união (int *A, int a, int *B, int b){
+    int u, j;
+    u = a + b;
+    int U[u];
+    for(i=0; i<a; i++){
+        for(j=0; j<b; j++){
+            if(A[i] == B[j]){
+                B[j] = -1;
+            }
+        }
+    }
+    for(i=0; i<a; i++){
+        U[i] = A[i];
+    }
+    for(i=0; i<b; i++){
+        U[i+a] = B[i];
+    }
+    printf("(");
+    for(i=0; i<u; i++){
+        if(U[i] != -1){
+            if(i>0 && i+1!=-1){
+                printf(",");
+            }
+            printf("%d", U[i]);
+        }
+    }
+    printf(")\n");
+}
+ 
+void interseção (int *A, int a, int *B, int b, int *I, int *con){
+    int x = 0;
+    if(a>b){
+        for(i=0; i<a; i++){
+            for(j=0; j<b; j++){
+                if(A[i] == B[j]){
+                    I[x++] = A[i];
+                }
+            }
+        }
+        *con = x;
+    }
+    if(a<b){
+        for(i=0; i<b; i++){
+            for(j=0; j<a; j++){
+                if(A[j] == B[i]){
+                    I[x++] = B[i];
+                }
+            }
+        }
+        *con = x;
+    }
+}
+}
